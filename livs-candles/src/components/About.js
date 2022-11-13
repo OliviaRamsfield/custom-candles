ba50ebd0fe95da175948b9fc79862e7c1e0b39ec
Olivia <ramsfol@gmail.com>
2022-11-13 08:46:28 -0600

    basic react app built

---
+++ livs-candles/src/components/About.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function About() {
+    return(
+        <section>
+            <h1>Who are we?</h1>
+            <p>placeholder text, placeholder text, placeholder text, placeholder text.</p>
+        </section>
+    )
+}
+
+export default About;
